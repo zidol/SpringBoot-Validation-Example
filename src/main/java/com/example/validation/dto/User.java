cc44e4f897c05ba087f25448b49ba509d910dd0e
zidol <ajh3166@naver.com>
2021-06-21 00:02:19 +0900

    @Valid

--- src/main/java/com/example/validation/dto/User.java
+++ src/main/java/com/example/validation/dto/User.java
@@ -1,6 +1,8 @@
 package com.example.validation.dto;
 
+import javax.validation.Valid;
 import javax.validation.constraints.*;
+import java.util.List;
 
 public class User {
 
@@ -10,19 +12,35 @@ public class User {
     @Max(value = 90)
     private int age;
 
-    @Email
-    private String email;
+//    @Email
+//    private String email;
+//
+//    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "핸드폰 번호의 양식과 맞지 않습니다. 01x-xxx(x)-xxxxx")
+//    private String  phoneNumber;
+//
+////    @Size(min = 6, max = 6)
+//    //(pattern = "yyyyMM")
+//    private String reqYearMonth; // yyyyMM;
 
-    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "핸드폰 번호의 양식과 맞지 않습니다. 01x-xxx(x)-xxxxx")
-    private String  phoneNumber;
-
-//    @Size(min = 6, max = 6)
-    //(pattern = "yyyyMM")
-    private String reqYearMonth; // yyyyMM;
+    @Valid // 다른 객체 선언하면 어노테이션 추가해줘야함 그래야 Car에 선언된 어노테이션이 적용됨
+    private List<Car> cars;
 
     public String getName() {
         return name;
     }
+
+
+
+    //    @AssertTrue(message = "yyyyMM 의 형식에 맞지 않습니다.")//메소드명 is로 시작해야함
+//    public boolean isReqYearMonthValidation() {
+//        try {
+//            LocalDate localDate = LocalDate.parse(this.reqYearMonth + "01", DateTimeFormatter.ofPattern("yyyyMMdd"));
+//        } catch (Exception e) {
+//            return false;
+//        }
+//        return true;
+//    }
+
 
     public void setName(String name) {
         this.name = name;
@@ -36,48 +54,20 @@ public class User {
         this.age = age;
     }
 
-    public String getEmail() {
-        return email;
+    public List<Car> getCars() {
+        return cars;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
     }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getReqYearMonth() {
-        return reqYearMonth;
-    }
-
-    public void setReqYearMonth(String reqYearMonth) {
-        this.reqYearMonth = reqYearMonth;
-    }
-
-//    @AssertTrue(message = "yyyyMM 의 형식에 맞지 않습니다.")//메소드명 is로 시작해야함
-//    public boolean isReqYearMonthValidation() {
-//        try {
-//            LocalDate localDate = LocalDate.parse(this.reqYearMonth + "01", DateTimeFormatter.ofPattern("yyyyMMdd"));
-//        } catch (Exception e) {
-//            return false;
-//        }
-//        return true;
-//    }
 
     @Override
     public String toString() {
         return "User{" +
                 "name='" + name + '\'' +
                 ", age=" + age +
-                ", email='" + email + '\'' +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                ", reqYearMonth='" + reqYearMonth + '\'' +
+                ", cars=" + cars +
                 '}';
     }
 }
